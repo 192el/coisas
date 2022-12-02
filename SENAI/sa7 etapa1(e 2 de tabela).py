35d707a49b482aab4a30019accc82a9f31bd72d5
Samuel Martins Gonçalves <115180300+192el@users.noreply.github.com>
2022-12-02 13:14:04 -0300

    SA7

--- SENAI/sa7 etapa1(e 2 de tabela).py
+++ SENAI/sa7 etapa1(e 2 de tabela).py
@@ -2,6 +2,8 @@
 # entendam a minha lógica se algum colega vir isso no github por favor tente entender como eu fiz e tente fazer a sua
 # propria lógica se tiver alguma dúvida sobre a minha lógica manda mensagem no discord, ou me marca no servidor do
 # discord também pode mandar pelo WhatsApp, mas eu não olho tanto quanto o discord
+assento_reservado_pessoa = {}
+
 
 class sweetFlight:
     def __init__(self, n_de_assentos):  # cada instância dessa classe é um avião e os assentos começam do zero
@@ -19,17 +21,20 @@ class sweetFlight:
                 if self.dicionario_das_reservas[i] == 'assento livre':
                     quant_assentoslivres += 1
             if quant_assentoslivres == 0:  # checando se tem algum assento livre
-                print('todos os assentos estão ocupados')
+                print('não há assentos disponiveis para este avião')
             else:
                 print(f'os assentos são {self.dicionario_das_reservas}')
-                # se tiver um assento livre será mostrado os assentos
-                assento = int(input("digite o numero do assento que deseja reservar:\n"))
+                # se tiver pelo menos um assento livre será mostrado os assentos
+                nome = input("digite o nome do passageiro:\n")
+                assento = int(input("digite o numero do assento para reservar:\n"))
                 # pedindo a 'chave' (numero) do assento para reservar
                 if self.dicionario_das_reservas[assento] == 'assento livre':  # se o assento estiver livre;
-                    self.dicionario_das_reservas[
-                        assento] = 'assento reservado'  # o valor dele será mudado para 'assento reservado'
+                    self.dicionario_das_reservas[assento] = f'assento reservado por {nome}'
+                    assento_reservado_pessoa[f'Av n°{qual_aviao} Assento n° {assento}'] = nome
+                    # o valor dele será mudado para 'assento reservado por (nome da pessoa)'
+                    # e no outro dicionário o nome e qual assento esta reservado
                     print(self.dicionario_das_reservas[assento])  # retorna 'assento reservado'
-                elif self.dicionario_das_reservas[assento] == 'assento reservado':
+                else:
                     print('esse assento já está reservado')
         except:  # esse execept devolve uma mensagem de erro caso o programa tente(try) uma chave que não existe
             raise KeyError('esse não é um assento valido')
@@ -39,38 +44,65 @@ class sweetFlight:
 
 
 # daqui para frente é só coisa que nós já fizemos outras vezes, acredito que não seja necessario muitos comentarios
-listaAvioes = []
+listaAvioes = {}
 
 
 def criaraviao():
-    quant_avioes = int(input('digite quantos aviões quer registrar:\n '))
+    quant_avioes = int(input('digite quantos aviões serão registrados:\n '))
     return quant_avioes
 
 
 def cond():
-    condicao = int(input(
-        "digite [1] para registrar um avião, [2] para criar uma reserva em um avião especifico, [3] para ver as "
-        "reservas de um avião especifico [0] para sair:\n"))
+    condicao = int(input("digite [1] para registrar o numero de cada avião, [2] para registrar a quantidade de "
+                         "assentos em cada avião, [3] para reservar passagem, [4] para consultar por avião, "
+                         "[5] para consulta por passageiro, [6] para encerrar:\n"))
     return condicao
 
 
 condicao = cond()
-while condicao != 0:
+while condicao != 6:
     if condicao == 1:
         quant_avioes = criaraviao()
-        for i in range(len(listaAvioes), quant_avioes + len(listaAvioes)):
-            quant_assentos = int(input(f"digite quantos assentos o avião {i + len(listaAvioes)} tem:\n"))
-            listaAvioes.append(
-                sweetFlight(quant_assentos))  # isso cria uma instancia da classe sweetflight e coloca ela numa lista
-        condicao = cond()  # se der print nessa lista ele mostra onde na memória essa instancia está alocada
+        for i in range(quant_avioes):
+            try:
+                chave_aviao = int(input("digite o numero do avião:\n"))
+                listaAvioes[chave_aviao] = 'aguardando registro da quantidade de assentos'
+            except:
+                raise TypeError("só podemos registrar uma quantidade inteira de aviões")
+        condicao = cond()
     elif condicao == 2:
-        qual_aviao = int(input(f"digite o numero do avião (começando com 0):\n"))
-        listaAvioes[qual_aviao].reservar()  # utilizando a função reservar na instância especificada
+        lista_numero_avioes = list(listaAvioes)
+        while lista_numero_avioes != []:
+            try:
+                quant_assentos = int(input(f"digite o numero de assentos do avião n°{lista_numero_avioes[0]}:\n"))
+                listaAvioes[lista_numero_avioes[0]] = sweetFlight(quant_assentos)
+                lista_numero_avioes.pop(0)
+            except:
+                raise TypeError('apenas numeros inteiros serão aceitos')
         condicao = cond()
     elif condicao == 3:
+        avioesdisponiveis = []
+        keys = list(listaAvioes)
+        for i in range(len(listaAvioes)):
+            if listaAvioes[keys[i]] != 'aguardando registro da quantidade de assentos':
+                avioesdisponiveis.append(keys[i])
+        print(f'os aviões disponiveis são {avioesdisponiveis}')
+        qual_aviao = int(input("digite o numero do avião:\n"))
         try:
-            qual_aviao = int(input(f"digite o numero do avião (começando com 0):\n"))
+            listaAvioes[qual_aviao].reservar()
+            condicao = cond()
+        except:
+            KeyError("Este avião não existe")
+    elif condicao == 4:
+        try:
+            qual_aviao = int(input("digite o numero do avião:\n"))
             print(listaAvioes[qual_aviao].mostrar())
             condicao = cond()
         except:
             raise ValueError("esse avião não foi registrado")
+    elif condicao == 5:
+        nome = input("digite o nome do passageiro:\n")
+        reservas_do_individuo = [key for key in assento_reservado_pessoa if assento_reservado_pessoa[key] == nome]
+        print("os assentos reservados por esse passageiro são:")
+        print(reservas_do_individuo)
+        condicao = cond()
